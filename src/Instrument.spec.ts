b20953e97cb641bd562db9cb845166d6b1e47c6c
Simon Mika <simon@mika.se>
2018-05-28 20:11:14 +0200

    Changed data path and added expects.

--- src/Instrument.spec.ts
+++ src/Instrument.spec.ts
@@ -3,8 +3,12 @@ import "isomorphic-fetch"
 
 describe("Instrument", () => {
 	it("load", async () => {
-		const value = await Instrument.load("https://simonmika.github.io/stockint-data/instruments/SE0007692124/")
+		const value = await Instrument.load("https://stockint.github.io/data/instruments/SE0007692124/")
 		expect(value).toBeTruthy()
 		expect(value.name).toEqual("IMINT Image Intelligence AB")
+		expect(value.symbol).toEqual("IMINT")
+		expect(value.isin).toEqual("SE0007692124")
+		expect(value.list).toEqual("AktieTorget")
+		expect(value.currency).toEqual("SEK")
 	})
 })
